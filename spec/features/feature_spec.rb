a0325f70d5b9289c6c5b4a4114c0261644d47bee
Mara Wanot <m.d.wanot@hotmail.com>
2016-05-06 13:50:58 +0100

    filtering tags

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -32,10 +32,46 @@ feature 'add single tag to a link' do
     fill_in "tags", with: "education"
     click_button 'submit'
     link = Link.first
-    p link
     expect(link.tags.map(&:name)).to include 'education'
   end
 end
+
+feature 'filter by tags' do
+  before(:each) do
+    Link.create(title: "Google", url: "http://google.com/", tags: [Tag.first_or_create(name: 'education')])
+    Link.create(title: "Gymbox", url: "http://gymbox.com/", tags: [Tag.first_or_create(name: 'gym')])
+    Link.create(title: "Facebook", url: "http://facebook.com/", tags: [Tag.first_or_create(name: 'bubbles')])
+  end
+
+    scenario 'filter bubbles' do
+      visit '/tags/bubbles'
+
+      expect(page.status_code).to eq 200
+      within 'ul#links' do 
+      expect(page).not_to have_content('Google')
+      expect(page).to_not have_content('Gymbox')
+      expect(page).to have_content('Facebook')
+      expect(page).to have_content('bubbles')
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
